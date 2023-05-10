4130a4e553bbbe3da7beb7cc8448aa895cc9429c
Joshua Owusu <joshbillion1@gmail.com>
2023-05-10 14:34:37 +0000

    Commit auto_update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import Header from "./components/Header/Header";
 import css from "./styles/app.module.scss"
 
 const App = () => {
-  return <div className={css.container}></div>
-  <Header/>
+  return (
+    <div className={css.container}>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
