0315b8b928e61d5812507530a3a8e08b1a38fc29
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 17:15:56 +0000

    Commit auto_update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,16 +6,18 @@ const Hero = () => {
     <section className={`padding ${css.wrapper}`}>
         <div className={`innerWidth ${css.container}`}>
 
-
+                {/* Upper Elements */}
                 <div className={css.upperElements}>
 
                     <span>
                         Hey there, <br/>I am Joshua
                         </span>
                     <span>I turn design into code<br/>
-                    </span>
+                    And I love what I do</span>
                 </div>
                 <div className={css.lowerElements}></div>
+
+                {/* Lower elements */}
 
         </div>
     </section>
