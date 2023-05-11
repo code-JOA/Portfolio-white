bf0d72788bd2b8981c99b0f8bb65356cb68f367f
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 15:49:05 +0000

    Commit auto_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Hero from "./components"
+import Hero from "./components/Hero/Hero";
 import css from "./styles/app.module.scss"
 
 const App = () => {
