f501738a78a0f3e6b6fd555db0ad1c33f9c57297
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 10:37:06 +0000

    Commit auto_update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
         </ul>
 
         <div className={css.menuIcon}>
-          
+          onClick={{preventDefault()}}
           <BiMenuAltRight size={30} />
         </div>
       </div>
