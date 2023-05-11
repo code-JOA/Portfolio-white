5f9aa4369f54e3ac0b819b3f84589709bfad1e90
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 15:10:12 +0000

    Commit auto_update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import css from "./Header.module.scss";
 import { BiMenuAltRight, BiPhoneCall } from "react-icons/bi";
 import { motion } from "framer-motion";
