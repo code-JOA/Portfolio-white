0c328bbfa807a249cf5b112d6ebb0746dd009d91
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 14:58:59 +0000

    Commit auto_update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,57 +1,48 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import css from "./Header.module.scss";
-import { BiPhoneCall, BiMenuAltRight } from "react-icons/bi";
+import { BiMenuAltRight, BiPhoneCall } from "react-icons/bi";
 import { motion } from "framer-motion";
 import { getMenuStyles, headerVariants } from "../../utils/motion";
-import useOutsideAlerter from "../../hooks/useOutsideAlerter";
-import useHeaderShadow from "../../hooks/useHeaderShadow";
+import { }
 
 const Header = () => {
-  const menuRef = useRef(null);
   const [menuOpened, setMenuOpened] = useState(false);
-  const headerShadow = useHeaderShadow();
-
-  //to handle click outside of sidebar on mobile
-  useOutsideAlerter({
-    menuRef,
-    setMenuOpened,
-  });
+  const headerShadow = useHeaderShadow(false);
 
   return (
     <motion.div
-      variants={headerVariants}
       initial="hidden"
       whileInView="show"
-      className={`bg-primary paddings ${css.wrapper}`}
-      viewport={{ once: true, amount: 0.25 }}
+      variants={headerVariants}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
       style={{ boxShadow: headerShadow }}
     >
-      <div className={`innerWidth ${css.container} flexCenter`}>
+      <div className={`flexCenter innerWidth ${css.container}`}>
         <div className={css.name}>Joshua</div>
+
         <ul
-          className={`flexCenter ${css.menu}`}
-          ref={menuRef}
           style={getMenuStyles(menuOpened)}
+          className={`flexCenter ${css.menu}`}
         >
           <li>
-            <a href="#experties">Services</a>
+            <a href="">Services</a>
           </li>
           <li>
-            <a href="#work">Experience</a>
+            <a href="">Experience</a>
           </li>
           <li>
-            <a href="#portfolio">Portfolio</a>
+            <a href="">Portfolio</a>
           </li>
           <li>
-            <a href="#people">Testimonials</a>
+            <a href="">Testimonials</a>
           </li>
           <li className={`flexCenter ${css.phone}`}>
-            <p>+44 (746) 080 8996</p>
+            <p> +44 746 080 8996</p>
             <BiPhoneCall size={"40px"} />
           </li>
         </ul>
-
-        {/* for medium and small screens */}
+        {/* For only medium and small screens */}
         <div
           className={css.menuIcon}
           onClick={() => setMenuOpened((prev) => !prev)}
