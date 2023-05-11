2ab88c38d18fc4bc86807183f7a8a51894d9782c
Joshua Owusu <joshbillion1@gmail.com>
2023-05-11 17:30:44 +0000

    Commit auto_update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,8 +29,7 @@ const Hero = () => {
           </div>
           <div className={css.certificate}>
             <img src="./certificate.png" alt="" />
-            <span>Certified Professional</span><span></span>
-            <span></span>
+            <span>Certified Professional</span>
             <span>UI/UX Designer</span>
           </div>
         </div>
