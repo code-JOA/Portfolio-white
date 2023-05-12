6498e41b3baa0cb2cbb44a66e5e79c0bff5f4922
Joshua Owusu <joshbillion1@gmail.com>
2023-05-12 13:17:57 +0000

    Commit auto_update

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import css from './Experience.module.scss'
+
+const Experience = () => {
+  return (
+    <section className={css.wrapper}>
+      <div className={`paddings yPaddings flexCenter innerWidth ${css.container}`}>
+        Experience
+      </div>
+    </section>
+  );
+}
+
+export default Experience
