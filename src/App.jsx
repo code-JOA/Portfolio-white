0eaa8945bfaac76b1f751cca9bd2433e9e8ae38c
Joshua Owusu <joshbillion1@gmail.com>
2023-05-15 17:16:04 +0000

    Commit auto_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Experience from "./components/Experience/Experience";
-import Work from "./components/Work/Work";
+// import Work from "./components/Work/Work";
 import css from "./styles/app.module.scss";
 
 const App = () => {
@@ -16,24 +16,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-import React from 'react'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Work from './components/Work/Work'
-import css from './styles/App.module.scss'
-const App = () => {
-  //don't forget to add font link in index.html
-  return (
-    <div className={`bg-primary ${css.container}`}>
-      <Header/>
-      <Hero/>
-      <Experties/>
-      <Work/>
-    </div>
-  )
-}
-
-export default App
