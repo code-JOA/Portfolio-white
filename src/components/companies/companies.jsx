26b429aa812b3c3db9992f3e18e7953f42e7154d
Joshua Owusu <joshbillion1@gmail.com>
2023-05-15 11:57:15 +0000

    Commit auto_update

--- src/components/companies/companies.jsx
+++ src/components/companies/companies.jsx
@@ -1,23 +1,78 @@
-import React from 'react'
-import css from './Companies.module.scss'
-import { motion } from 'framer-motion'
-
-const Companies = () => {
+import React from "react";
+import { workExp } from "../../utils/data";
+import css from "./Companies.module.scss";
+import { motion } from "framer-motion";
+import {
+  draw,
+  fadeIn,
+  slideIn,
+  staggerChildren,
+  textVariant2,
+  zoomIn,
+} from "../../utils/motion";
+const Work = () => {
   return (
     <motion.section
       variants={staggerChildren}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`padding ${css.wrapper}`}
+      className={`paddings ${css.wrapper}`}
     >
-      <div className={css.container}>
-        <span className="primaryText">
-            My Work Experience
-        </span>
+      <a className="anchor" id="work"></a>
+
+      <div className={`innerWidth flexCenter ${css.container}`}>
+        {/* heading */}
+        <span className="primaryText yPaddings">My Work Experience</span>
+
+        <div className={`flexCenter ${css.experiences}`}>
+          {workExp.map((exp, i) => {
+            return (
+              <motion.div
+                variants={textVariant2}
+                key={i}
+                className={`flexCenter ${css.exp}`}
+              >
+                <div className={css.post}>
+                  <h1>{exp.place}</h1>
+                  <p>{exp.tenure}</p>
+                </div>
+                <div className={css.role}>
+                  <h1>{exp.role}</h1>
+                  <p>{exp.detail}</p>
+                </div>
+              </motion.div>
+            );
+          })}
+
+          <motion.div variants={zoomIn(1, 1)} className={css.progressbar}>
+            <motion.div
+              variants={fadeIn("down", "tween", 2, 1.5)}
+              className={css.line}
+            ></motion.div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#286F6C" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#F2704E" }}
+              ></div>
+            </div>
+            <div>
+              <div
+                className={css.circle}
+                style={{ background: "#EEC048" }}
+              ></div>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </motion.section>
   );
-}
+};
 
-export default Companies
+export default Work;
