3e2b58ad2bacacaa1a6548e8eb8c9886d71b6a94
Joshua Owusu <joshbillion1@gmail.com>
2023-05-15 14:08:09 +0000

    Commit auto_update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import Experience from "./components/Experience/Experience"
 import Companies from "./components/Companies/Companies"
