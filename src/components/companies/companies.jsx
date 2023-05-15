ae077887b14625bfad7117abe9a09e4d26b4ce4e
Joshua Owusu <joshbillion1@gmail.com>
2023-05-15 11:44:19 +0000

    Commit auto_update

--- src/components/companies/companies.jsx
+++ src/components/companies/companies.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import css from './Companies.module.scss'
+import motion } from 'framer-motion'
 
 const companies = () => {
   return (
     <section className={`padding ${css.wrapper}`}>
 
 
-        div.{css}
+        <div className={css.container}>
+            blabla
+        </div>
     </section>
     )
 }
