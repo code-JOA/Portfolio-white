53315659e104dfbedf80ba41422b7d03c6e60f89
Joshua Owusu <joshbillion1@gmail.com>
2023-05-16 22:36:28 +0000

    Commit auto_update

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -39,26 +39,17 @@ const Work = () => {
             );
           })}
 
-          {/* <motion.div variants={zoomIn(1,1)} className={css.progressBar}>
-          <motion.div variants={fadeIn("down", "tween", 2, 1.5)} className={css.line}></motion.div>
-
-            <div><div className={css.circle} style={{background: "#286F6C"}}></div></div>
-            <div><div className={css.circle} style={{background: "#F2704E"}}></div></div>
-            <div><div className={css.circle} style={{background: "#EEC048"}}></div></div>
-        </motion.div>
-      </div>
-      </div>
- 
-    </motion.section> */}
 
           <motion.div variants={zoomIn(1, 1)} className={css.progressbar}>
             <motion.div variants={fadeIn("down", "tween", 2, 1.5)} className={css.line}></motion.div>
+
           <div><div className={css.circle} style={{ background: "#286F6C" }}></div></div>
             <div><div className={css.circle} style={{ background: "#F2704E" }}></div></div>
             <div><div className={css.circle} style={{ background: "#EEC048" }}></div></div>
           </motion.div>
         </div>
       </div>
+
     </motion.section>
   );
 };
