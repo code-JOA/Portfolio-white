39f4eb9ed742a54eeea9d969ba661d119f45425d
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 16:35:07 +0000

    Commit auto_update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero/Hero";
 import Experience from "./components/Experience/Experience";
 import Work from "./components/Work/Work";
 import Portfolio from "./components/Portfolio/Portfolio";
-
+import People from './components/People/People';
 import css from "./styles/app.module.scss";
 // import { motion } from 'framer-motion'
 
