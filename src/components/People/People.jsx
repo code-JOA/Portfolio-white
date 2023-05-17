f41d0c244ee1c5e0556c78df239d325e8a63b874
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 18:01:50 +0000

    Commit auto_update

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -14,12 +14,13 @@ const People = () => {
     >
       <div className={`yPaddings innerWidth ${css.container}`}>
         <div className={`flexCenter ${css.heading}`}>
-          <span className="primaryText">
-            Testimonials
-            <p>
-                I contacted Josh on LinkedIn and he helped me write my masters thesis.
-            </p>
-          </span>
+          <span className="primaryText">Testimonials</span>
+
+          <p>
+            I contacted Josh on LinkedIn and he helped me write my masters
+            thesis.
+          </p>
+          <p></p>
         </div>
       </div>
     </motion.section>
