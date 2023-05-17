306b9fc64abc110f03584954da9b8b1ddbc3a17e
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 13:52:48 +0000

    Commit auto_update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import css from './Portfolio.module.scss'
 
 const Portfolio.module = () => {
   return (
     <section className={`paddings ${css.wrapper}`}>
 
-        <div className={css.exp}>
+        <div className={css.container}>
 
         </div>
     </section>
