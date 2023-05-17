16e66cd801a0b8b47796559a7025af5a1967b49d
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 13:48:39 +0000

    Commit auto_update

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Portfolio.module = () => {
+  return (
+    <section className={css.wrapper}>
+
+        <div className={css.exp}>
+
+        </div>
+    </section>
+      )
+}
+
+export default Portfolio.module
