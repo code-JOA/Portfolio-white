65400b0fbf624a45b905450e2f5894e6dd223fd9
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 16:07:29 +0000

    Commit auto_update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -12,8 +12,13 @@ const Portfolio = () => {
       viewport={{ once: false, amount: 0.25 }}
       className={`paddings ${css.wrapper}`}
     >
+      <a className="anchor" id="portfolio"></a>
+
       <div className={`innerWidth flexCenter ${css.container}`}>
-        <motion.div variants={textVariant(0.4)} className={`flexCenter ${css.heading}`}>
+        <motion.div
+          variants={textVariant(0.4)}
+          className={`flexCenter ${css.heading}`}
+        >
           <div>
             <span className="primaryText">My Latest Project</span>
             <p style={{ marginTop: "10px" }}>
