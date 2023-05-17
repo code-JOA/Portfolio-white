495f273fa878eecaaa8d203201ca251349c0b3ca
Joshua Owusu <joshbillion1@gmail.com>
2023-05-17 15:48:41 +0000

    Commit auto_update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import css from './Portfolio.module.scss'
 import { motion } from 'framer-motion'
-import { fadeIn } from '../../utils/motion'
+import { fadeIn, staggerChildren } from '../../utils/motion'
 
 const Portfolio = () => {
   return (
