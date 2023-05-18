601537cb55884b0ccfe7e3d3f9f4e7168aab49b6
Joshua Owusu <joshbillion1@gmail.com>
2023-05-18 06:45:26 +0000

    Commit auto_update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,27 +4,22 @@ import css from './Footer.module.scss'
 const Footer = () => {
   return (
     <section className={css.wrapper}>
+      <div className={css.container}>
+        <div className={css.left}>
+          <span className="primaryText">
+            Let's Build Something <br />
+            Awesome Together
+          </span>
 
-        <div className={css.container}>
-
-            <div className={css.left}>
-
-                <span className="primaryText">
-                    Let's Build Together
-                </span>
-
-                <span className="primaryText">
-                    Feel Free <a href="@mailto:joshbillion1@gmail.com" >Say Hello </a>
-
-                </span>
-            </div>
-
-            <div className={css.right}>
-
-            </div>
+          <span className="primaryText">
+            Feel Free <a href="@mailto:joshbillion1@gmail.com">Say Hello </a>
+          </span>
         </div>
+
+        <div className={css.right}></div>
+      </div>
     </section>
-    )
+  );
 }
 
 export default Footer
