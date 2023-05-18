b2c8e1dc29ffe66cb1b3276eab737db004d3bf90
Joshua Owusu <joshbillion1@gmail.com>
2023-05-18 16:38:35 +0000

    Commit auto_update

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
-import css from './Work.module.scss'
-import { motion } from 'framer-motion'
-// import { draw, staggerChildren, textVariant2 , zoomIn, fadeIn} from '../../utils/motion'
-import { fadeIn, slideIn, staggerChildren, textVariant2, zoomIn} from "../../utils/motion"
+// import React from 'react'
+// import css from './Work.module.scss'
+// import { motion } from 'framer-motion'
+// // import { draw, staggerChildren, textVariant2 , zoomIn, fadeIn} from '../../utils/motion'
+// import { fadeIn, slideIn, staggerChildren, textVariant2, zoomIn} from "../../utils/motion"
 
-import { workExp } from '../../utils/data'
+// import { workExp } from '../../utils/data'
 
+// const Work = () => {
+//   return (
+//     <motion.section
+//       variants={staggerChildren}
+//       initial="hidden"
+//       whileInView="show"
+//       viewport={{ once: false, amount: 0.25 }}
+//       className={`paddings ${css.wrapper}`}
+//     >
+//       <a className="anchor" id="work"></a>
+//       <div className={`innerWidth flexCenter ${css.container}`}>
+//         <span className="primaryText yPaddings">Work Experience</span>
+
+//         <div className={`flexCenter ${css.experiences}`}>
+//           {workExp.map((exp, i) => {
+//             return (
+//               <motion.div
+//                 variants={textVariant2}
+//                 key={i}
+//                 className={`flexCenter ${css.exp}`}
+//               >
+//                 <div className={css.post}>
+//                   <h1>{exp.place}</h1>
+//                   <p>{exp.tenure}</p>
+//                 </div>
+
+//                 <div className={css.role}>
+//                   <h1>{exp.role}</h1>
+//                   <p>{exp.detail}</p>
+//                 </div>
+//               </motion.div>
+//             );
+//           })}
+
+//           <motion.div variants={zoomIn(1, 1)} className={css.progressbar}>
+//             <motion.div
+//               variants={fadeIn("down", "tween", 2, 1.5)}
+//               className={css.line}
+//             ></motion.div>
+
+//             <div>
+//               <div
+//                 className={css.circle}
+//                 style={{ background: "#286F6C" }}
+//               ></div>
+//             </div>
+//             <div>
+//               <div
+//                 className={css.circle}
+//                 style={{ background: "#F2704E" }}
+//               ></div>
+//             </div>
+//             <div>
+//               <div
+//                 className={css.circle}
+//                 style={{ background: "#EEC048" }}
+//               ></div>
+//             </div>
+//           </motion.div>
+//         </div>
+//       </div>
+//     </motion.section>
+//   );
+// };
+
+// export default Work
+
+
+
+import React from "react";
+import { workExp } from "../../utils/data";
+import css from "./Work.module.scss";
+import { motion } from "framer-motion";
+import {
+  draw,
+  fadeIn,
+  slideIn,
+  staggerChildren,
+  textVariant2,
+  zoomIn,
+} from "../../utils/motion";
 const Work = () => {
   return (
     <motion.section
@@ -16,8 +97,10 @@ const Work = () => {
       className={`paddings ${css.wrapper}`}
     >
       <a className="anchor" id="work"></a>
+
       <div className={`innerWidth flexCenter ${css.container}`}>
-        <span className="primaryText yPaddings">Work Experience</span>
+        {/* heading */}
+        <span className="primaryText yPaddings">My Work Experience</span>
 
         <div className={`flexCenter ${css.experiences}`}>
           {workExp.map((exp, i) => {
@@ -31,7 +114,6 @@ const Work = () => {
                   <h1>{exp.place}</h1>
                   <p>{exp.tenure}</p>
                 </div>
-
                 <div className={css.role}>
                   <h1>{exp.role}</h1>
                   <p>{exp.detail}</p>
@@ -45,7 +127,6 @@ const Work = () => {
               variants={fadeIn("down", "tween", 2, 1.5)}
               className={css.line}
             ></motion.div>
-
             <div>
               <div
                 className={css.circle}
@@ -71,4 +152,4 @@ const Work = () => {
   );
 };
 
-export default Work
+export default Work;
