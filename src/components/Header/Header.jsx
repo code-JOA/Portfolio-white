1112dd30a398a535e23fe7f477d5893f8592a083
Joshua Owusu <joshbillion1@gmail.com>
2023-05-18 17:03:07 +0000

    Commit auto_update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
       <div className={`innerWidth ${css.container} flexCenter`}>
         <div className={css.name}>joshua.io</div>
 
-        <ul
+        <ul ref="menuRef"
           style={getMenuStyles(menuOpened)}
           className={`flexCenter ${css.menu}`}
         >
