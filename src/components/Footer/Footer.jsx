bf8549d1f09b6b496bfaa9faf4da7609fde60832
Joshua Owusu <joshbillion1@gmail.com>
2023-05-18 15:49:30 +0000

    Commit auto_update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,51 @@
-// 
+import React from 'react';
+import css from './Footer.module.scss';
+import { footerVariants, staggerChildren } from '../../utils/motion';
+import { motion } from 'framer-motion';
 
-
-import React from "react";
-import { footerVariants, staggerChildren } from "../../utils/motion";
-import css from "./Footer.module.scss";
-import { motion } from "framer-motion";
 const Footer = () => {
   return (
     <motion.section
-      variants={staggerChildren}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`paddings ${css.wrapper}`}
-    >
-      <motion.div
+    vairiants={staggerChildren}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25 }}
+    className={`paddings ${css.wrapper}`}>
+
+        <motion.div
         variants={footerVariants}
-        className={`innerWidth yPaddings flexCenter ${css.container}`}
-      >
+        className={`innerWidth yPaddings flexCenter ${css.container}`}>
         <div className={css.left}>
-          <span className="primaryText">
-            Let's make something <br />
-            amazing together.
+            <span className="primaryText">
+            Let's Build Something <br />
+            Awesome Together
           </span>
+
           <span className="primaryText">
-            Start by <a href="mailto:zainkeepscode@gmail.com">saying hi</a>
+            Feel Free <a href="@mailto:joshbillion1@gmail.com">Say Hello </a>
           </span>
         </div>
 
         <div className={css.right}>
-          <div className={css.info}>
-            <span className="secondaryText">Information</span>
-            <p>145 New York, FL 5467, USA</p>
-          </div>
-          <ul className={css.menu}>
-            <li>Services</li>
-            <li>Works</li>
-            <li>Notes</li>
-            <li>Experience</li>
-          </ul>
+
+            <div className={css.info}>
+
+                <span className="secondaryText">
+                    Contact
+                </span>
+
+                <p>484 Kings Road, London</p>
+            </div>
+            <ul>
+                <li>Services</li>
+                <li>Works</li>
+                <li>Notes</li>
+                <li>Experience</li>
+            </ul>
         </div>
       </motion.div>
     </motion.section>
   );
-};
+}
 
-export default Footer;
+export default Footer
